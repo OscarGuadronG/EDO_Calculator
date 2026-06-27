6d99703e90cff1f61bffd186629056a27a05d7dd
oscarguadron <gg24017@gmail.com>
2026-06-26 18:20:14 -0600

    AhoraSiUltimoCommit

--- controller/multipasos_controller.py
+++ controller/multipasos_controller.py
@@ -1,26 +1,26 @@
-from model.methods.multipasos.adams_bashfoth import adams_bashforth_2_method
-from model.methods.multipasos.adams_bashfoth import adams_bashforth_3_method
-from model.methods.multipasos.adams_bashfoth import adams_bashforth_4_method
-from model.methods.multipasos.Adams_Moulton import adams_moulton_2_method
-from model.methods.multipasos.Adams_Moulton import adams_moulton_3_method
-from model.methods.multipasos.Adams_Moulton import adams_moulton_4_method
+from model.methods.multipasos.adams_bashfoth.adams_bashforth_2_method import AdamsBashforth2Method
+from model.methods.multipasos.adams_bashfoth.adams_bashforth_3_method import AdamsBashforth3Method
+from model.methods.multipasos.adams_bashfoth.adams_bashforth_4_method import AdamsBashforth4Method
+from model.methods.multipasos.Adams_Moulton.adams_moulton_2_method import AdamsMoulton2Method
+from model.methods.multipasos.Adams_Moulton.adams_moulton_3_method import AdamsMoulton3Method
+from model.methods.multipasos.Adams_Moulton.adams_moulton_4_method import AdamsMoulton4Method
 from model.methods.multipasos.predictor_corrector import PredictorCorrectorMethod
 
 class MultipasosController:
 
     def execute(self, method, f, x0, y0, h, n, xf, **keywargs):
         if method == "ab2":
-            solver = adams_bashforth_2_method(f, x0, y0, h, n, xf)
+            solver = AdamsBashforth2Method(f, x0, y0, h, n, xf)
         if method == "ab3":
-            solver = adams_bashforth_3_method(f, x0, y0, h, n, xf)
+            solver = AdamsBashforth3Method(f, x0, y0, h, n, xf)
         if method == "ab4":
-            solver = adams_bashforth_4_method(f, x0, y0, h, n, xf)
+            solver = AdamsBashforth4Method(f, x0, y0, h, n, xf)
         if method == "am2":
-            solver = adams_moulton_2_method(f, x0, y0, h, n, xf)
+            solver = AdamsMoulton2Method(f, x0, y0, h, n, xf)
         if method == "am3":
-            solver = adams_moulton_3_method(f, x0, y0, h, n, xf)
+            solver = AdamsMoulton3Method(f, x0, y0, h, n, xf)
         if method == "am4":
-            solver = adams_moulton_4_method(f, x0, y0, h, n, xf)
+            solver = AdamsMoulton4Method(f, x0, y0, h, n, xf)
         if method == "predictor_corrector":
             solver = PredictorCorrectorMethod(f, x0, y0, h, n, xf)
         else:
